48a2de51712671860ec87726c480fe3b50637182
Edith Amondi <eamondi014@gmail.com>
2025-06-11 13:40:33 +0300

    Fix: made nav-links function work

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -21,10 +22,9 @@ const links = [
 ];
 
 export default function NavLinks() {
- 
   const pathname = usePathname();
-  
-}
+
+  console.log('pathname', pathname)
   return (
     <>
       {links.map((link) => {
@@ -33,7 +33,11 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+             {
+              'bg-sky-100 text-blue-600' : pathname === link.href
+             }, 
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
@@ -41,4 +45,5 @@ export default function NavLinks() {
         );
       })}
     </>
-  
+  );
+}
